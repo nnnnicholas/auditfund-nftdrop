b11400a6443f24afb29f63894dba53cab249579d
nicholas <nnnnicholas@users.noreply.github.com>
2022-10-11 19:35:06 -0500

    change names

---
+++ test/NFT.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
+
+contract NFTTest is Test {
+    NFT public nft;
+
+    function setUp() public {}
+
+    function testIncrement() public {}
+
+    function testSetNumber(uint256 x) public {}
+}
